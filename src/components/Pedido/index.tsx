607ff22bcab5bbf2c32d2a8809742a4660f409d7
Gabriel Ribeiro Da Silva <gabriel.ribeiro@oi.net.br>
2022-04-05 22:42:23 -0300

    Ajuste na exibição dos dados

--- src/components/Pedido/index.tsx
+++ src/components/Pedido/index.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { createElement, useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import styles from "./Pedido.module.css";
 
 import "react-loading-skeleton/dist/skeleton.css";
+import { contentPedido } from "../../db";
 
 export function Pedido() {
   const [show, setShow] = useState(false);
@@ -51,59 +52,13 @@ export function Pedido() {
         </section>
       ) : (
         <section className={styles.container}>
-          <h4>Tenho certeza que acertou tudo, mas o resultado é o de menos!</h4>
-
-          <p>
-            O que importa realmente é a parceria, amor e reciprocidade entre a
-            gente, um sempre ao lado do outro pro que der e vier.
-          </p>
-
-          <p>
-            Talvez não seja uma surpresa para você, eu também não sei se já
-            esperava, mas vamos nessa kk. Pensei em várias formas para fazer
-            isso, mas acho que a maneira mais autêntica seria usando alguma
-            coisa que eu domino, que é codando kkk.
-          </p>
-
-          <p>
-            Completamos hoje 5 anos juntos e só tenho a agradecer a Deus, pois
-            todo momento do seu lado só me faz sentir o homem mais sortudo do
-            mundo! Tantas momento que já passamos, tantas fases, evoluções e
-            conquistas e nós lá sempre apoiando um ao outro.
-          </p>
-
-          <p>
-            Tudo começou naquela van, um local diferente para se conhecer
-            alguém, voltando tarde da noite, a maioria das vezes cansados e com
-            sono, mas aconteceu, precisou de um empurrãozinho, mas deu certo.
-            Fomos conversando, a coisa foi evoluindo e o amor foi crescendo.
-          </p>
-
-          <p>
-            O tempo de faculdade acabou, mas o amor e o companheirismo só cresce
-            desde então.
-          </p>
-
-          <p>
-            Ah, uma curiosidade bacana kk. Sabe as flores que te faço com
-            guardanapo sempre que vamos a uma lanchonete? Não sei se já te
-            contei, mas passei horas treinando vendo vídeos no youtube só pra
-            fazer o mais bonito possível, e valeu a pena, pois o sorriso que
-            você me dá toda vez torna o meu dia perfeito 💓
-          </p>
-
-          <p>
-            É legal ver o quanto amadurecemos. Não eramos muito de fazer planos,
-            e olha como estamos agora.
-          </p>
-
-          <p>
-            Sabe que te amo muito, mas nunca me canso de dizer, e quero passar o
-            resto da minha vida com você, e dito isso, acho que é momento de dar
-            um novo passo, então escreva aí pra mim:
-          </p>
-
-          <h3>ACEITA SE CASAR COMIGO?</h3>
+          {contentPedido.map(pedido => {
+            return createElement(
+              pedido.tag,
+              {},
+              pedido.content
+            )
+          })}
 
           <input
             type="text"
--- src/db.js
+++ src/db.js
@@ -61,3 +61,46 @@ export const questions = [
     answers: ["Calor", "Frio"],
   },
 ];
+
+export const contentPedido = [
+  {
+    tag: "h4",
+    content: "Tenho certeza que acertou tudo, mas o resultado é o de menos!"
+  },
+  {
+    tag: "p",
+    content: "O que importa realmente é a parceria, amor e reciprocidade entre a gente, um sempre ao lado do outro pro que der e vier."
+  },
+  {
+    tag: "p",
+    content: "Talvez não seja uma surpresa para você, eu também não sei se já esperava, mas vamos nessa kk. Pensei em várias formas para fazer isso, mas acho que a maneira mais autêntica seria usando alguma coisa que eu domino, que é codando kkk."
+  },
+  {
+    tag: "p",
+    content: "Completamos hoje 5 anos juntos e só tenho a agradecer a Deus, pois todo momento do seu lado só me faz sentir o homem mais sortudo do mundo! Tantas momento que já passamos, tantas fases, evoluções e conquistas e nós lá sempre apoiando um ao outro."
+  },
+  {
+    tag: "p",
+    content: "Tudo começou naquela van, um local diferente para se conhecer alguém, voltando tarde da noite, a maioria das vezes cansados e com sono, mas aconteceu, precisou de um empurrãozinho, mas deu certo. Fomos conversando, a coisa foi evoluindo e o amor foi crescendo."
+  },
+  {
+    tag: "p",
+    content: "O tempo de faculdade acabou, mas o amor e o companheirismo só cresce desde então."
+  },
+  {
+    tag: "p",
+    content: "Ah, uma curiosidade bacana kk. Sabe as flores que te faço com guardanapo sempre que vamos a uma lanchonete? Não sei se já te contei, mas passei horas treinando vendo vídeos no youtube só pra fazer o mais bonito possível, e valeu a pena, pois o sorriso que     você me dá toda vez torna o meu dia perfeito 💓"
+  },
+  {
+    tag: "p",
+    content: "É legal ver o quanto amadurecemos. Não eramos muito de fazer planos, e olha como estamos agora."
+  },
+  {
+    tag: "p",
+    content: "Sabe que te amo muito, mas nunca me canso de dizer, e quero passar o resto da minha vida com você, e dito isso, acho que é momento de dar um novo passo, então escreva aí pra mim:"
+  },
+  {
+    tag: "h3",
+    content: "ACEITA SE CASAR COMIGO?"
+  }
+]
